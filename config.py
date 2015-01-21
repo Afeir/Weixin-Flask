66d76824cbae45d049e63a6f53a9e4f30f96a722
Archer_小A <qddegtya@gmail.com>
2015-01-21 18:28:38 +0800

    Update config.py
    更新config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # -*- coding:utf-8 -*-
 # config
 CONFIG = {
-    "app_id": "wxecddbf8a08d50caa",  # app_id
-    "secret": "a92518c68b5d7918cfdd0988682e3186",  # secret
-    "client_credential": "client_credential"  # client_credential
+    "app_id": "",  # app_id 公众号app_id
+    "secret": "",  # secret 公众号secret
+    "client_credential": "client_credential"  # client_credential 这块默认不要动
 }
